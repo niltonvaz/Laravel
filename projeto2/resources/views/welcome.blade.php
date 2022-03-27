1d972e0108cc7a26cb078ddbb5768e5cbbf3c697
niltonvaz <nilton_vaz@msn.com>
2022-03-27 08:16:39 -0300

    correção de nome da model e do arquivo Model para Models

--- projeto2/resources/views/welcome.blade.php
+++ projeto2/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @foreach($events as $event)
-<p>Não funcionaaaaaaaaaaaaaaaaaaaa por queeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee kkkk</p>
+
 <p>{{ $event->titulo }} -- {{ $event->descricao }}</p>
 
 @endforeach
